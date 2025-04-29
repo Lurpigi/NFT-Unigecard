79b347e8db6eca4caaa28fd003d4a4a307f02f66
Lurpigi <luigiestebant@gmail.com>
2025-04-30 00:53:23 +0200

    Updates

--- infUnigeNFT/lib/stuff.js
+++
@@ -1,8 +0,0 @@
-import { toast } from 'vue-sonner'
-
-export function showError (title, e) {
-    if (e && e.reason){
-      toast.error(title, { description: e.reason , duration: 5000 });
-      console.error(e);
-    } else toast.error(title, { duration: 5000 });
-  }
